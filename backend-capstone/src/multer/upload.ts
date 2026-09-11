7affa94dab3525a26217b5a72adc0913b0d76f7a
neiltruong <truongtu678@gmail.com>
2026-09-12 01:36:27 +0700

    fix: static uploads directory pathing

--- backend-capstone/src/multer/upload.ts
+++ backend-capstone/src/multer/upload.ts
@@ -2,7 +2,8 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const uploadDir = "uploads";
+// ✅ Dùng process.cwd() để ép đường dẫn luôn tuyệt đối về folder /uploads ở root project
+const uploadDir = path.join(process.cwd(), "uploads");
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, {
